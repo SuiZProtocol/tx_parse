1fefad970d0ec19bd53a2847e0b7bf3cff9abd06
0xCryptoZen <1315275049@qq.com>
2025-02-22 23:27:25 +0800

    ✨ feat: Update package.json for UMD/DTS build & vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,31 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  build: {
+    lib: {
+      // 使用 resolve 确保路径正确
+      entry: resolve(__dirname, './src/index.ts'),
+      name: 'SuiTxParse',
+      fileName: 'index',
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['@mysten/sui'],
+      output: {
+        globals: {
+          '@mysten/sui': 'Sui'
+        }
+      }
+    }
+  },
+  plugins: [
+    dts({ 
+      insertTypesEntry: true,
+      rollupTypes: true
+    })
+  ],
   test: {
     globals: true,
     environment: "node",
